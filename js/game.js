84b45cdd85f71e1bf273a32d3e65da58f1bea651
Vince Campanale <vcampanale0294@gmail.com>
2017-01-12 13:50:35 -0500

    game pattern vs. player pattern check working

--- js/game.js
+++ js/game.js
@@ -9,18 +9,45 @@ const Game = {
   start: function start() {
     UI.deactivateStartButton();
     Game.incrementCount(); //increment the count to 1
-    Game.gamePattern.push(Game.generateRandomNumber());//generate a random number and load that number into the pattern
-    console.log(Game.gamePattern);
-    Game.playPattern([2,4,1]);
+    Game.gamePattern.push(Game.generateRandomNumber()); //generate a random number and load that number into the pattern (Game.generateRandomNumber())
+    Game.playPattern(Game.gamePattern);
+    Game.playerTurn(); //TODO: Set timeout so this doesn't run until playPattern function is completed
   },
   incrementCount: function incrementCount() {
     Game.count++;
     UI.updateCountDisplay();
   },
-  //allows user to click pads
-  handlePlayerClick: function handlePlayerClick() {
+  //allows user to click pads and compare their clicks to the game pattern
+  playerTurn: function playerTurn() {
     //add click event listeners to all the pads
-    //record the number of the clicked pad (push it to playPattern)
+    const pads = document.querySelectorAll('.pad');
+    pads.forEach(pad => pad.addEventListener('click', handleClick));
+
+    //private function for Game.playerTurn() which handles clicks
+    function handleClick() {
+      const pad = this; //set context to variable "pad" - makes it easier to reference
+      const padNumber = parseInt(pad.getAttribute("data-pad")); //get the pad number from "data-pad" attribute
+      Game.playerPattern.push(padNumber); //push the pad number to the player pattern
+
+      //check if click is correct
+      Game.currentIndex++; //increment index
+      if (Game.playerPattern[Game.currentIndex] == Game.gamePattern[Game.currentIndex]) { //if playerPattern matches gamePattern at current index
+        Pad.play(padNumber); //play the pad
+        if (Game.playerPattern.length === Game.gamePattern.length) { //if the player has completed the game pattern
+          setTimeout(() => Game.incrementCount(), 250); //pause for a moment before incrementing the count
+          Game.gamePattern.push(Game.generateRandomNumber()); //add new pad to game pattern
+          Game.playerPattern = []; //reset player pattern
+          Game.currentIndex = -1;
+          setTimeout(() => Game.playPattern(Game.gamePattern), 1250); //wait 1250ms, then play the new game pattern
+        }
+      } else {
+        Pad.errorSound(); // if it isn't, play the error sound
+        //check for strict mode
+      }
+
+
+
+    }
     //if click is correct (if playerPattern[index] = currentPattern[index]),
       //play the sound
       //if playerPattern.length === currentPattern.length
@@ -41,6 +68,7 @@ const Game = {
     padNumbers.forEach(function(padNumber, index) {
       setTimeout(Pad.play.bind(null, padNumber), index * interval);
     });
+    //after the pattern plays, listen for the player's input
   },
   //set interval in between each sound/light based on the length of the pattern array
   setInterval: function setInterval(patternLength) {
@@ -60,6 +88,11 @@ const Game = {
     const randomNumber = Math.floor(Math.random() * 4) + 1; //pick integer between 1 and 4
     return randomNumber;
   },
+  //(only fires when strict mode is toggled) when the user makes an error, set count to 1 and start over
+  strictModeError: function strictModeError() {
+
+  },
+  //reset the game entirely (only fires when off button is clicked)
   reset: function reset() {
     UI.strictLED.className = "led"; //turn off strictLED light
     Game.count = 0; //set count back to 0
@@ -83,6 +116,11 @@ const Pad = {
     sound.play(); //play the sound
 
     Pad.revertToNormal(); //after the pad does it's thing, revert it back to normal
+  },
+  errorSound: function errorSound() {
+    // const sound = document.querySelector(); //find error sound to load
+    // sound.play();
+    console.log("Error! Wrong pad.");
   },
   //revert the pad back to its normal size and color (remove the "playing" class)
   revertToNormal: function revertToNormal() {
