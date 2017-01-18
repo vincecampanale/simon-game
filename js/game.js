210f7e324648c008610f4d2b07389cf9806fbfac
Vince Campanale <vcampanale0294@gmail.com>
2017-01-17 21:19:45 -0500

    separated pad into its own file

--- js/game.js
+++ js/game.js
@@ -49,8 +49,8 @@ const Game = {
           Game.restartPlayerTurn(); // if not strict mode, just restart the current turn (don't reset count)
         } else {
           Pad.wrongPad(padNumber);
-          setTimeout(() => Game.restartGame(), 500); //if strict mode, reset count to 01, restart game
-          setTimeout(() => Game.playPattern(Game.gamePattern), 1000);
+          setTimeout(() => Game.restartGame(), 500); //if strict mode, reset count to 01, reset patterns0
+          setTimeout(() => Game.playPattern(Game.gamePattern), 1000); //play the game pattern after a small delay
         }
       }
     }
@@ -107,42 +107,5 @@ const Game = {
     Game.playerPattern = []; //clear player pattern
     Game.gamePattern = []; //clear game pattern
     Game.currentIndex = -1;
-  }
-}
-
-const Pad = {
-  //play the pad (light it up and make the accompanying sound)
-  play: function play(padNumber) {
-    const sound = document.querySelector(`audio[data-sound="${padNumber}"]`);
-    const pad = document.querySelector(`div[data-pad="${padNumber}"]`);
-
-    if (!sound || !pad) return; //if number is invalid, stop the function from running altogether
-
-    pad.classList.add('playing'); //make the pad a little bigger and light up
-
-    sound.currentTime = 0; //rewind sound to the start to be safe
-    sound.play(); //play the sound
-
-    Pad.revertToNormal(); //after the pad does it's thing, revert it back to normal
-  },
-  wrongPad: function wrongPad(padNumber) {
-    const sound = document.querySelector(`audio[data-sound="err"]`); //get error sound from html
-    const pad = document.querySelector(`div[data-pad="${padNumber}"]`); //get pad
-
-    pad.classList.add('playing'); //light the pad up
-    sound.play(); //play error sound
-
-    UI.errorDisplay(); //display "!!" in count
-    setTimeout(() => UI.updateCountDisplay(), 1000);
-  },
-  //revert the pad back to its normal size and color (remove the "playing" class)
-  revertToNormal: function revertToNormal() {
-    const pads = document.querySelectorAll('.pad');
-    pads.forEach(pad => pad.addEventListener('transitionend', Pad.removeTransition));
-  },
-  //helper function to remove the transition from the pad. Used in 'revertToNormal' function
-  removeTransition: function removeTransition(e) {
-    if(e.propertyName !== 'transform') return; //only pay attention to the end of an event with propertyName "transform"
-    this.classList.remove('playing');
   }
 }
---
+++ js/pad.js
@@ -0,0 +1,36 @@
+const Pad = {
+  //play the pad (light it up and make the accompanying sound)
+  play: function play(padNumber) {
+    const sound = document.querySelector(`audio[data-sound="${padNumber}"]`);
+    const pad = document.querySelector(`div[data-pad="${padNumber}"]`);
+
+    if (!sound || !pad) return; //if number is invalid, stop the function from running altogether
+
+    pad.classList.add('playing'); //make the pad a little bigger and light up
+
+    sound.currentTime = 0; //rewind sound to the start to be safe
+    sound.play(); //play the sound
+
+    Pad.revertToNormal(); //after the pad does it's thing, revert it back to normal
+  },
+  wrongPad: function wrongPad(padNumber) {
+    const sound = document.querySelector(`audio[data-sound="err"]`); //get error sound from html
+    const pad = document.querySelector(`div[data-pad="${padNumber}"]`); //get pad
+
+    pad.classList.add('playing'); //light the pad up
+    sound.play(); //play error sound
+
+    UI.errorDisplay(); //display "!!" in count
+    setTimeout(() => UI.updateCountDisplay(), 1000);
+  },
+  //revert the pad back to its normal size and color (remove the "playing" class)
+  revertToNormal: function revertToNormal() {
+    const pads = document.querySelectorAll('.pad');
+    pads.forEach(pad => pad.addEventListener('transitionend', Pad.removeTransition));
+  },
+  //helper function to remove the transition from the pad. Used in 'revertToNormal' function
+  removeTransition: function removeTransition(e) {
+    if(e.propertyName !== 'transform') return; //only pay attention to the end of an event with propertyName "transform"
+    this.classList.remove('playing');
+  }
+}
